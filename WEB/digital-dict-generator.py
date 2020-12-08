145840d6e64b0a365f01c5e63028b5fdf68130b3
ybdt <65813455+ybdt@users.noreply.github.com>
2020-12-08 12:56:12 +0800

    Update digital-dict-generator.py

--- WEB/digital-dict-generator.py
+++ WEB/digital-dict-generator.py
@@ -1,16 +1,13 @@
-var = "0000";
+#!/usr/bin/python3
 
-with open("dict.txt", "a") as f:
-    while int(var) <= 9999:
-        if 0 <= int(var) <= 8:  #此处如果是9的话，生成的10会变成100
-            f.write(var + "\n");
-            var = "000" + str(int(var) + 1);
-        elif 9 <= int(var) <= 98:  #此处如果是99的话，生成的100会变成1000
-            f.write(var + "\n");
-            var = "00" + str(int(var) + 1);
-        elif 99 <= int(var) <= 998:  #此处如果是999的话，生成的1000会变成10000
-            f.write(var + "\n");
-            var = "0" + str(int(var) + 1);
-        else:
-            f.write(var + "\n");
-            var = str(int(var) + 1);
+def main():
+    with open("dict.txt", "w") as f:
+        for i in range(0, 1000000):
+            len_for_i = len( str(i) );
+            if len_for_i != 6:
+                new_i = "0" * (6-len_for_i) + str(i);
+                f.write(new_i + "\n");
+            else:
+                f.write(str(i) + "\n");
+
+main();
