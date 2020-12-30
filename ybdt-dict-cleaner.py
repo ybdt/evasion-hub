0dada9c7ac043dbb05af0459b6bf78f81c835b4e
ybdt <65813455+ybdt@users.noreply.github.com>
2020-12-30 11:47:28 +0800

    Update ybdt-dict-cleaner.py

--- ybdt-dict-cleaner.py
+++ ybdt-dict-cleaner.py
@@ -2,40 +2,28 @@
 
 import sys
 
-f = open("ybdt-full-dict.txt", "a");#if not exist, create it, if exist, do nothing
-f.close();
-
 def cut_repeat(dict):
-    f_new = open(dict, "r");
-    f_old = open("ybdt-full-dict.txt", "r");
-    tmp = f_old.readlines();
-    print( "The old file count is: " + str(len(tmp)) );
-    #print("The old file is:");
-    #print(tmp);
-    tmp_ = [];#create an empty array to store element without \n
-    for line in tmp:
-        tmp_.append( line.strip("\n") );
-    lines = f_new.readlines();
-    print("The new file count is: " + str(len(lines)) );
-    #print("The new file is:");
-    #print(lines);
-    count = 0;#counter for repeated element
-    print("The repeated element is: ");
-    for line in lines:
-        if line.strip("\n") not in tmp_:
-            tmp_.append( line.strip("\n") );
-        else:
-            print( line.strip("\n") );
-            count = count + 1;
-    print( "The repeated count is: " + str(count) );
-    print( "The final count is: " + str(len(tmp_)) );
-    f_new.close();
-    f_old.close();
-    
-    f = open("ybdt-full-dict.txt", "w");
-    for line in tmp_:
-        f.write(line + "\n");
-    f.close();
+    with open(dict, "r", encoding="gb2312") as f_original:
+        dict_name = dict.split(".")[0]
+        with open(dict_name + "-new" + ".txt", "a") as f_new:
+            lines = f_original.readlines();
+            print( "The original file count is: " + str( len(lines) ) );
+            
+            #创建一个临时数组，存储每行中去掉换行符后的字符部分
+            tmp = [];
+
+            #每次添加到数组前，比对是否在数组中出现过，没出现过则添加到数组中
+            for line in lines:
+                if line.strip("\n") not in tmp:
+                    tmp.append( line.strip("\n") );
+                else:
+                    continue;
+            
+            print( "The new file count is: " + str( len(tmp) ) );
+
+            #将去重后的临时数组写入新的文件
+            for item in tmp:
+                f_new.write(item + "\n");
 
 def main():
     if len(sys.argv) != 2:
