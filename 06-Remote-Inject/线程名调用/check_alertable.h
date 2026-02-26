bf179aafbab17b217a61a3c87669818cc45dbae7
ybdt <714161644@qq.com>
2026-02-26 15:54:07 +0800

    1

--- 06-Remote-Inject/线程名调用/check_alertable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <vector>
-
-// Required hProcess with PROCESS_DUP_HANDLE privilege
-HANDLE find_alertable_by_event(HANDLE hProcess, std::vector<DWORD>& threads);
-
